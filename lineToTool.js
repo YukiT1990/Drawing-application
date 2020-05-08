b042b2ac7b52c6bbbae4ada0633521ccbc7cad57
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2020-05-08 12:04:19 +0900

    Drawing application

---
+++ lineToTool.js
@@ -0,0 +1,37 @@
+function LineToTool(){
+	this.icon = "assets/lineTo.jpg";
+	this.name = "LineTo";
+
+	var startMouseX = -1;
+	var startMouseY = -1;
+	var drawing = false;
+
+	this.draw = function(){
+
+		if(mouseIsPressed){
+			if(startMouseX == -1){
+				startMouseX = mouseX;
+				startMouseY = mouseY;
+				drawing = true;
+//              Loads the pixels data for this image into the [pixels] attribute.
+				loadPixels();
+			}
+
+			else{
+//              Updates the backing canvas for this image with the contents of the [pixels] array.
+
+				updatePixels();
+				line(startMouseX, startMouseY, mouseX, mouseY);
+			}
+
+		}
+
+		else if(drawing){
+			drawing = false;
+			startMouseX = -1;
+			startMouseY = -1;
+		}
+	};
+
+
+}
